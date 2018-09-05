0cb8c944cf53140be84c59d1072bd2413d973e70
icecoobe <icecoobe@qq.com>
2018-09-05 16:55:44 +0800

    Add facade project

---
+++ Facade/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DesignPattern.Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example in C#
+            Facade facade = new Facade();
+
+            facade.MethodA();
+            facade.MethodB();
+            Console.WriteLine("------------------------------");
+
+            // Example in real-world
+            Customer user = new Customer("Luke");
+            Mortgage mortgage = new Mortgage();
+            bool eligible = mortgage.IsEligible(user, 3000);
+            Console.WriteLine("\n" + user.Name + " has been " + (eligible ? "Approved" : "Rejected"));
+            Console.WriteLine("------------------------------");
+
+            ShapeMaker maker = new ShapeMaker();
+            maker.Draw();
+            Console.WriteLine("------------------------------");
+        }
+    }
+}
---
+++ Facade/ShapeMaker.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Facade
+{
+    class ShapeMaker
+    {
+        private List<IShape> shapes = new List<IShape>();
+
+        public ShapeMaker()
+        {
+            shapes.Add(new Circle());
+            shapes.Add(new Rectangle());
+        }
+
+        public void Draw()
+        {
+            foreach (IShape s in shapes)
+                s.Draw();
+        }
+    }
+
+    interface IShape
+    {
+        void Draw();
+    }
+
+    class Circle : IShape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Draw a Circle.");
+        }
+    }
+
+    class Rectangle : IShape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Draw a Rectangle.");
+        }
+    }
+}
